5bdd6d9f34c6440f19bf5c6b24f8f85fc1b0b591
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-11-09 18:30:10 +0000

    create .github/workflows/ci.yml #2

---
+++ frontend/src/Components/Note/Note.tsx
@@ -0,0 +1,11 @@
+import api from "../../Utils/api";
+import { useEffect, useState } from "react";
+
+type NoteProps = {
+    id: string,
+}
+
+export default function Note({ id }: NoteProps) {
+
+
+}
