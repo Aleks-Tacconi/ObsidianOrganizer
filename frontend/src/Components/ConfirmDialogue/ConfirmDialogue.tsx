c2bd895cbee5372b11bde4bd89c2c7f6ce04333e
Aleks Tacconi <aleks.tacconi@gmail.com>
2026-02-01 14:12:19 +0000

    #15 Update URLS to be modifiable after adding them

--- frontend/src/Components/ConfirmDialogue/ConfirmDialogue.tsx
+++ frontend/src/Components/ConfirmDialogue/ConfirmDialogue.tsx
@@ -1,5 +1,4 @@
 import { Dialog, DialogTitle, DialogContent, Button, Typography } from "@mui/material";
-import { FaCheck, FaTimes } from "react-icons/fa";
 
 type ConfirmDialogueProps = {
   open: boolean;
@@ -30,10 +29,8 @@ export default function ConfirmDialogue({
       <DialogContent sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 1 }}>
         <Typography>{message}</Typography>
         <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end", gap: 5 }}>
-          <Button startIcon={<FaTimes />} variant="outlined" onClick={onDecline}>
-            Decline
-          </Button>
-          <Button startIcon={<FaCheck />} variant="contained" onClick={onConfirm}>
+          <Button onClick={onDecline}>Decline</Button>
+          <Button variant="contained" onClick={onConfirm}>
             Confirm
           </Button>
         </div>
